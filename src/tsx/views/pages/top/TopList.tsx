b8f2b72084b9b5a5c3c3122f489260c4c530c298
仲本勇斗 <tenisu0303@icloud.com>
2021-02-07 23:58:49 +0900

    study day3

--- src/tsx/views/pages/top/TopList.tsx
+++ src/tsx/views/pages/top/TopList.tsx
@@ -1,6 +1,8 @@
 // 同ページ内のユニークな要素は同じディレクトリで作成
 import { jsx } from '@emotion/react'
 import { hot } from 'react-hot-loader'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Detail from '../views/pages/detail/Detail'
 
 const TopList = ({ user }) => {
   return (
@@ -8,7 +10,9 @@ const TopList = ({ user }) => {
       <ul>
         {user.length &&
           user.map(item => (
-            <li key={item.id}>{item.firstName}</li>
+            <li key={item.id} id={item.id}>
+              <Route path="/detail/:id" component={Detail}>{item.firstName}</Route>
+            </li>
           ))
         }
       </ul>
